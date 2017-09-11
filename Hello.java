9dc65193519da6f483472b671b6f246c3c0f7ce7
pmgilmore41 <pmgilmore41@email.arizona.edu>
2017-09-11 11:20:36 -0700

    red leader over

--- Hello.java
+++ Hello.java
@@ -9,7 +9,7 @@ public class Hello {
     }
 
     public static void callSign() {
-        
+        system.out.println("Lock s-foils in attack position.");
         // Red Leader should add a print statement that says "Lock S-foils in attack position." 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
@@ -22,7 +22,8 @@ public class Hello {
 
     }
 
-    public static void redLeader() {
+    public static void redLeader() { 
+	    system.out.println("I copy, gold leader!");
  
         // Red Leader should add a print statement that says "I copy, gold leader!"
         
