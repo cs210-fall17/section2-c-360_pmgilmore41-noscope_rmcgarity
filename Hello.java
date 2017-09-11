82f4967f0e5e358afad34b811eb42cb93e8cfa89
rileymcaz <rmcgarity@email.arizona.edu>
2017-09-11 11:30:26 -0700

    fixed mistakes

--- Hello.java
+++ Hello.java
@@ -9,21 +9,21 @@ public class Hello {
     }
 
     public static void callSign() {
-        system.out.println("Lock s-foils in attack position.");
+        System.out.println("Lock s-foils in attack position.");
         // Red Leader should add a print statement that says "Lock S-foils in attack position." 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
-	system.out.println("Stabilize your rear deflectors... Watch for enemy fighters.");
+	System.out.println("Stabilize your rear deflectors... Watch for enemy fighters.");
     }
 
     public static void goldLeader() {
-	system.out.println("Loosen up!");
+	System.out.println("Loosen up!");
         // Gold Leader should add a print statement that says "Loosen up!"
     
 
     }
 
     public static void redLeader() { 
-	    system.out.println("I copy, gold leader!");
+	    System.out.println("I copy, gold leader!");
  
         // Red Leader should add a print statement that says "I copy, gold leader!"
         
